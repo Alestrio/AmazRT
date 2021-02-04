3062ac432a2b6a20cff034996fc4f6b6129dc6ed
Meryem KAYA <meryem.kaya@etudiant.univ-reims.fr>
2021-02-04 16:08:54 +0100

    Création du dossier projetflask

---
+++ projetflask/prj_flask/__init__.py
@@ -0,0 +1,4 @@
+from flask import Flask
+from projetflask.prj_flask import routes
+app = Flask(__name__)
+
---
+++ projetflask/run.py
@@ -0,0 +1,3 @@
+from projetflask.prj_flask import app
+if __name__ == "__main__":
+    app.run(debug=True)

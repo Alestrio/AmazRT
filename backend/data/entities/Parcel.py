11583b14b53cf92318e1df422d2fc59bb5db5d1c
Alestrio <lebalexis@gmail.com>
2021-02-09 10:59:53 +0100

    [Entities] Data models

--- backend/data/entities/Parcel.py
+++ backend/data/entities/Parcel.py
@@ -5,6 +5,8 @@
 #  - Meryem KAYA @MeryemKy
 #  - Alexis LEBEL @Alestrio
 #  - Malo LEGRAND @HoesMaaad
+from sqlalchemy import Integer, Column, String, Float, Boolean, Date
+
 from backend.data.entities.AbstractEntity import AbstractEntity
 
 """
@@ -16,6 +18,20 @@ It contains all the informations related to the parcel in itself (not the tracki
 
 
 class Parcel(AbstractEntity):
+    id = Column(Integer, primary_key=True)
+    reference = Column('reference', String(32))
+    condition = Column('condition', String(32))
+    weight = Column('weight', Float)
+    length = Column('length', Float)
+    width = Column('width', Float)
+    height = Column('height', Float)
+    sender = Column('sender', String(128))
+    receiver = Column('receiver', String(128))
+    dangerous = Column('dangerous', Boolean)
+    content = Column('content', String(128))
+    deposit_date = Column('deposit_date', Date)
+    destination_pld = Column('destination_pld', String(32))
+    current_position = Column('current_position', String(32))
 
     """
     Constructor:
@@ -35,7 +51,7 @@ class Parcel(AbstractEntity):
     def __init__(self, serial: int, reference: str, condition: str, weight: float,
                  length: float, width: float, height: float, sender: str, receiver: str,
                  dangerous: bool, content: str, deposit_date: str, destination_pld, current_position):
-        super(Parcel, self).__init__(serial)
+        super().__init__(serial)
         self.reference = reference
         self.condition = condition
         self.weight = weight
--- backend/data/entities/Tracking.py
+++ backend/data/entities/Tracking.py
@@ -5,6 +5,9 @@
 #  - Meryem KAYA @MeryemKy
 #  - Alexis LEBEL @Alestrio
 #  - Malo LEGRAND @HoesMaaad
+from sqlalchemy import Column, Integer, ARRAY, String, Date
+from sqlalchemy.orm import relationship
+
 from backend.data.entities.AbstractEntity import AbstractEntity
 
 """
@@ -15,6 +18,10 @@ This is the data model used for Parcel tracking
 
 
 class Tracking(AbstractEntity):
+    id = Column(Integer, primary_key=True)
+    parcel = relationship("Parcel")
+    successive_positions = Column('successive_positions', ARRAY(String(32)))
+    successive_date = Column('successive_dates', ARRAY(Date))
 
     """
     Constructor
@@ -25,9 +32,10 @@ class Tracking(AbstractEntity):
     - successive_positions : an array of string containing the PLD the parcel tripped to and from
     - successive_dates : an array of strings containing the date the parcel moved to another position
     """
-    def __init__(self, serial: int, tracking_number: str, successive_positions, successive_dates):
-        self.tracking_number = tracking_number
+    def __init__(self, serial: int, parcel, successive_positions, successive_dates):
+        self.parcel = parcel
         self.successive_positions = successive_positions
+        self.successive_dates = successive_dates
         super().__init__(serial)
 
     """This is the method adding a positions to the tracking successive positions"""

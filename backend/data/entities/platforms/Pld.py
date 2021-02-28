c83f9226c28bbdf58ab084dce9066b3f451a6111
Alestrio <lebalexis@gmail.com>
2021-02-28 20:31:25 +0100

    [BACKEND] Created PLD Class

--- backend/data/entities/platforms/Pld.py
+++ backend/data/entities/platforms/Pld.py
@@ -5,4 +5,28 @@
 #  - Meryem KAYA @MeryemKy
 #  - Alexis LEBEL @Alestrio
 #  - Malo LEGRAND @HoesMaaad
+from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy.orm import relationship
 
+from backend.data.entities.AbstractEntity import AbstractEntity
+
+
+class Pld(AbstractEntity):
+    """
+    @Entity
+    This is the entity class responsible for department-level platform data management.
+    The tablename is "pld"
+    """
+    __tablename__ = 'pld'
+    id_pld = Column('id_pld', Integer, primary_key=True)
+    id_plr = relationship('plr', foreign_keys='plr.id_plr')
+    ref = Column('ref_pld', VARCHAR(6))
+    name = Column('nom_pld', VARCHAR(50))
+
+    def __init__(self,
+                 id_pld, id_plr, ref, name):
+        super().__init__()
+        self.id_pld = id_pld
+        self.id_plr = id_plr
+        self.ref = ref
+        self.name = name

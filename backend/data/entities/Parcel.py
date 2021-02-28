8c94def7d75cfd9c7b678ddccca7da5e38d26e64
Alestrio <lebalexis@gmail.com>
2021-02-28 20:46:52 +0100

    [BACKEND] Created Parcel Class

--- backend/data/entities/Parcel.py
+++ backend/data/entities/Parcel.py
@@ -5,4 +5,25 @@
 #  - Meryem KAYA @MeryemKy
 #  - Alexis LEBEL @Alestrio
 #  - Malo LEGRAND @HoesMaaad
+from sqlalchemy import Column, Integer, VARCHAR
 
+from backend.data.entities.AbstractEntity import AbstractEntity
+
+
+class Parcel(AbstractEntity):
+    """
+    @Entity
+    This is the entity class responsible for supplier data management.
+    The tablename is "fournisseur"
+    """
+    __tablename__ = 'colis'
+    id_parcel = Column('id_colis', Integer, primary_key=True)
+    ref = Column('ref_colis', VARCHAR(30))
+    type = Column('type_colis', VARCHAR(20))
+
+    def __init__(self,
+                 id_parcel, ref, ptype):
+        super().__init__()
+        self.id_parcel = id_parcel
+        self.ref = ref
+        self.type = ptype

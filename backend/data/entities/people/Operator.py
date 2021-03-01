5593eb2caf2aa8c494db65e481ce032de7de2918
Alestrio <lebalexis@gmail.com>
2021-03-01 08:40:04 +0100

    [BACKEND] Created Operator class

--- backend/data/entities/people/Operator.py
+++ backend/data/entities/people/Operator.py
@@ -5,4 +5,32 @@
 #  - Meryem KAYA @MeryemKy
 #  - Alexis LEBEL @Alestrio
 #  - Malo LEGRAND @HoesMaaad
+from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy.orm import relationship
 
+from backend.data.entities.AbstractEntity import AbstractEntity
+
+
+class Operator(AbstractEntity):
+    """
+    @Entity
+    This is the entity class responsible for operator data management.
+    The tablename is "personnel"
+    """
+    __tablename__ = 'personnel'
+    id_operator = Column('id_personnel', Integer, primary_key=True)
+    id_pld = relationship('pld', foreign_keys='pld.is_pld')
+    lastname = Column('nom_personnel', VARCHAR(50))
+    firstname = Column('prenom_personnel', VARCHAR(50))
+    login = Column('login_personnel', VARCHAR(15))
+    password = Column('mdp_personnel', VARCHAR(15))
+
+    def __init__(self,
+                 id_operator, id_pld, lastname, firstname, login, password):
+        super().__init__()
+        self.id_operator = id_operator
+        self.id_pld = id_pld
+        self.lastname = lastname
+        self.firstname = firstname
+        self.login = login
+        self.password = password

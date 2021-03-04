27dd24f98a061bf7418936ca2c81777f118fe18a
Alestrio <lebalexis@gmail.com>
2021-03-04 16:58:01 +0100

    [BACKEND.Operator] Adjusted operator according to test

--- backend/data/entities/people/Operator.py
+++ backend/data/entities/people/Operator.py
@@ -5,7 +5,7 @@
 #  - Meryem KAYA @MeryemKy
 #  - Alexis LEBEL @Alestrio
 #  - Malo LEGRAND @HoesMaaad
-from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy import Column, Integer, VARCHAR, ForeignKey
 from sqlalchemy.orm import relationship
 
 from backend import Base
@@ -19,16 +19,14 @@ class Operator(Base):
     """
     __tablename__ = 'personnel'
     id_operator = Column('id_personnel', Integer, primary_key=True)
-    id_pld = relationship('pld', foreign_keys='pld.is_pld')
+    id_pld = Column('id_pld', Integer, ForeignKey('pld.id_pld'))
     lastname = Column('nom_personnel', VARCHAR(50))
     firstname = Column('prenom_personnel', VARCHAR(50))
     login = Column('login_personnel', VARCHAR(15))
     password = Column('mdp_personnel', VARCHAR(15))
 
     def __init__(self,
-                 id_operator, id_pld, lastname, firstname, login, password):
-        super().__init__()
-        self.id_operator = id_operator
+                 id_pld, lastname, firstname, login, password):
         self.id_pld = id_pld
         self.lastname = lastname
         self.firstname = firstname

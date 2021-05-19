28348e3dc6d5cbfb5677ef4c90f755fa3cda54aa
Alestrio <lebalexis@gmail.com>
2021-05-19 10:59:26 +0200

    [BACKEND.Entities] Edited Customer with only relevant information

--- application/data/entities/people/Customer.py
+++ application/data/entities/people/Customer.py
@@ -23,18 +23,16 @@ class Customer(Base, UserMixin):
     ref = Column('ref_client', VARCHAR(15))
     lastname = Column('nom_client', VARCHAR(30))
     firstname = Column('prenom_client', VARCHAR(30))
-    address = Column('adresse_client', VARCHAR(100))
     login = Column('login_client', VARCHAR(15))
     password = Column("mdp_client", VARCHAR(255))
 
     def __init__(self,
-                 id_city, ref, lastname, firstname, address, login, password):
+                 id_city, ref, lastname, firstname, login, password):
         """Constructor"""
         self.id_city = id_city
         self.ref = ref
         self.lastname = lastname
         self.firstname = firstname
-        self.address = address
         self.login = login
         self.password = password
 

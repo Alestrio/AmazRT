f74d8ac8d96dedef0604c802b00f87a4137abe19
Alexis LEBEL <lebalexis@gmail.com>
2021-06-08 21:28:27 +0200

    [APPLICATION.Run] Changed load user function callback

--- application/run.py
+++ application/run.py
@@ -9,22 +9,22 @@ import os
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
 from flask_qrcode import QRcode
+from flask import session
 
 from application import app
-from application.data.base import Base, engine, Session
+from application.data.ApiService import ApiService
 from application.data.entities.people.Customer import Customer
 from application.data.entities.people.Operator import Operator
 from application.data.entities.people.Supplier import Supplier
 
 login = LoginManager()
 
-
 @login.user_loader
 def load_user(u_login):
     user = {
-        'as_customer': session.query(Customer).filter_by(login=u_login).first(),
-        'as_operator': session.query(Operator).filter_by(login=u_login).first(),
-        'as_supplier': session.query(Supplier).filter_by(login=u_login).first()
+        'as_customer': Customer.fromdict(Customer.filter_by(service.getall(Customer()), login=u_login)),
+        'as_operator': Operator.fromdict(Operator.filter_by(service.getall(Operator()), login=u_login)),
+        'as_supplier': Supplier.fromdict(Supplier.filter_by(service.getall(Supplier()), login=u_login))
     }
     for i in user:
         if user[i] is not None:
@@ -32,24 +32,22 @@ def load_user(u_login):
     return None
 
 
+
+
 if __name__ == "__main__":
     csrf = CSRFProtect(app)
     SECRET_KEY = os.urandom(32)
     app.config['SECRET_KEY'] = SECRET_KEY
     app.config['WTF_CSRF_SECRET_KEY'] = SECRET_KEY
-    session = Session()
-    Base.metadata.create_all(engine)
     csrf.init_app(app)
     login.init_app(app)
     QRcode(app)
-    app.run(debug=True)
+    app.run(debug=True, port=80)
 else:
     csrf = CSRFProtect(app)
     SECRET_KEY = os.urandom(32)
     app.config['SECRET_KEY'] = SECRET_KEY
     app.config['WTF_CSRF_SECRET_KEY'] = SECRET_KEY
-    session = Session()
-    Base.metadata.create_all(engine)
     csrf.init_app(app)
     login.init_app(app)
     QRcode(app)

c0bc8c45705ea37d0f8d40d557c888393cdae604
Alexis LEBEL <lebalexis@gmail.com>
2021-06-14 10:03:02 +0200

    [APPLICATION.Register] cronjob shell

---
+++ util/newfile_cronjob.sh
@@ -0,0 +1,12 @@
+#
+#   AmazRT  -  Parcel Management System
+#   First semester Technical Degree project
+#     Copyright  (c) 2021 - 2022
+#    - Meryem KAYA @MeryemKy
+#    - Alexis LEBEL @Alestrio
+#    - Malo LEGRAND @HoesMaaad
+#
+
+export PYTHONPATH=/home/amazrt/AmazRT/
+source /home/amazrt/AmazRT/venv/bin/activate
+python3 /home/amazrt/Amazrt/util/check_received_parcel.py "$1"
